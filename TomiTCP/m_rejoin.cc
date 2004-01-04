10c07276794d00e398762e2f1426266699003079
Tomas Janousek <tomi@nomi.cz>
2004-01-04 20:15:43 +0000

    m_channel sleep on rejoin
    git-svn-id: file:///tmp/svn/TomiTCP/head@15 0366d8a9-9ecf-0310-8b56-9fddfa06f24e

--- TomiTCP/m_rejoin.cc
+++ TomiTCP/m_rejoin.cc
@@ -7,13 +7,14 @@ extern "C" {
 void m_rejoin_msg(FILE *f, string snick, string shost, vector<string> cmd)
 {
     if (!strcasecmp(cmd[0].c_str(),"KICK") && !strcasecmp(cmd[2].c_str(),nick.c_str())) {
-	    S(f,"JOIN %s\n",cmd[1].c_str());
+	sleep(5);
+	S(f,"JOIN %s\n",cmd[1].c_str());
     }
 
     if ((cmd[0] == "471" || cmd[0] == "473" || 
 		cmd[0] == "474" || cmd[0] == "475")
 	    && !strcasecmp(cmd[1].c_str(),nick.c_str())) {
-	sleep(1);
+	sleep(5);
 	S(f,"JOIN %s\n",cmd[2].c_str());
     }
 }
