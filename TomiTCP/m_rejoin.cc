4b3c51277e03f882a929feffdeedb5b45ced19bf
Tomas Janousek <tomi@nomi.cz>
2004-01-04 20:04:14 +0000

    rejoin channel module
    git-svn-id: file:///tmp/svn/TomiTCP/head@13 0366d8a9-9ecf-0310-8b56-9fddfa06f24e

---
+++ TomiTCP/m_rejoin.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "irc.h"
+using namespace std;
+
+extern "C" {
+
+void m_rejoin_msg(FILE *f, string snick, string shost, vector<string> cmd)
+{
+    if (!strcasecmp(cmd[0].c_str(),"KICK") && !strcasecmp(cmd[2].c_str(),nick.c_str())) {
+	    S(f,"JOIN %s\n",cmd[1].c_str());
+    }
+
+    if ((cmd[0] == "471" || cmd[0] == "473" || 
+		cmd[0] == "474" || cmd[0] == "475")
+	    && !strcasecmp(cmd[1].c_str(),nick.c_str())) {
+	S(f,"JOIN %s\n",cmd[2].c_str());
+    }
+}
+
+void m_rejoin_init(struct module &m)
+{
+    cout << "m_rejoin init - hello" << endl;
+    m.msg = m_rejoin_msg;
+}
+
+}
