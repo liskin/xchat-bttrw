1be6bf20809cd970bb16589d66ef67fbc3814ea0
Tomas Janousek <tomi@nomi.cz>
2004-01-04 21:38:05 +0000

    rj_sleep conf. option
    git-svn-id: file:///tmp/svn/TomiTCP/head@18 0366d8a9-9ecf-0310-8b56-9fddfa06f24e

--- TomiTCP/m_rejoin.cc
+++ TomiTCP/m_rejoin.cc
@@ -3,19 +3,31 @@
 #include "irc.h"
 using namespace std;
 
+int rj_sleep = 5;
+
 extern "C" {
+
+int m_rejoin_config(string a, string b)
+{
+    if (!strcasecmp(a.c_str(),"rj_sleep"))
+        rj_sleep = atol(b.c_str());
+    else
+        return 1;
+
+    return 0;
+}
 
 void m_rejoin_msg(FILE *f, string snick, string shost, vector<string> cmd)
 {
     if (!strcasecmp(cmd[0].c_str(),"KICK") && !strcasecmp(cmd[2].c_str(),nick.c_str())) {
-	sleep(5);
+	sleep(rj_sleep);
 	S(f,"JOIN %s\n",cmd[1].c_str());
     }
 
     if ((cmd[0] == "471" || cmd[0] == "473" || 
 		cmd[0] == "474" || cmd[0] == "475")
 	    && !strcasecmp(cmd[1].c_str(),nick.c_str())) {
-	sleep(5);
+	sleep(rj_sleep);
 	S(f,"JOIN %s\n",cmd[2].c_str());
     }
 }
@@ -24,6 +36,7 @@ void m_rejoin_init(struct module &m)
 {
     cout << "m_rejoin init - hello" << endl;
     m.msg = m_rejoin_msg;
+    m.config = m_rejoin_config;
 }
 
 }
