60fd559e7df46299afa4ae65c99b22cc835e8b55
Tomas Janousek <tomi@nomi.cz>
2004-07-31 20:08:41 +0000

    Forgot to add these files.
    git-svn-id: file:///tmp/svn/tomi/xchat-bttrw@127 b4258ba7-4dd6-0310-8e73-d8b1db2f4ef9
    
    :000000 100644 0000000... e67974b... A	idle.c
    :000000 100644 0000000... 7d6a44b... A	idle.h

---
+++ idle.c
@@ -0,0 +1,7 @@
+#include "idle.h"
+
+const char *idle_msgs[] = {
+    "Spravna krychle tuhne rychle."
+};
+
+const int idle_msgs_count = sizeof(idle_msgs)/sizeof(*idle_msgs);
---
+++ idle.h
@@ -0,0 +1,7 @@
+#ifndef _IDLE_H_INCLUDED
+#define _IDLE_H_INCLUDED
+
+extern const char *idle_msgs[];
+extern const int idle_msgs_count;
+
+#endif /* _IDLE_H_INCLUDED */
