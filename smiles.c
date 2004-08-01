55f1b0de542c9335b27d76ee7c627280f0fd229b
Tomas Janousek <tomi@nomi.cz>
2004-08-01 18:15:54 +0000

    Added some smilies.
    git-svn-id: file:///tmp/svn/tomi/xchat-bttrw@143 b4258ba7-4dd6-0310-8e73-d8b1db2f4ef9
    
    :100644 100644 ec1d58c... 2c88174... M	smiles.c

--- smiles.c
+++ smiles.c
@@ -2,7 +2,50 @@
 
 const char *smiles[] = {
     [1] ":D",
-    [2] ":)"
+    [2] ":))",
+    [3] ":DD",
+    [4] ";)",
+    [5] "8)",
+    [6] ":)",
+    [7] ":|",
+    [8] ":/",
+    [9] ":o",
+    [10] ":(",
+    [11] ">:(",
+    [12] ":'(",
+    [13] "*andel/dabel*",
+    [14] "*smoking*",
+    [15] "*srdce*",
+    [16] "*kytka*",
+    [17] ":{",
+    [18] "=)",
+    [19] "=|",
+    [20] ":.)",
+    [21] "*mimo*",
+    [22] "*pivo*",
+    [23] ":-x",
+//    [24] "", // fialovej toci ocima
+    [25] "*zamilovane_oci*",
+    [26] "*mmnt*",
+    [27] "*amor*",
+    [28] "*zarofka*",
+    [29] "*hodiny*",
+//    [30] "", // rve a krici
+    [31] "*slunicko*",
+    [32] "*oblizovak*",
+    [33] "*telefon*",
+    [34] "*paranoidni*",
+    [35] "*kafe*",
+//    [36] "", // toci ocima
+    [37] ":-E",
+    [38] "*zmotanej_xicht*",
+    [39] ">E",
+    [40] ":-F",
+    [43] "8",
+    [55] "*vozickar*",
+    [64] "*pichacka*",
+    [87] "*mava*",
+    [99] "*zelva_ninja*"
 };
 
 const int smiles_count = sizeof(smiles)/sizeof(*smiles);
