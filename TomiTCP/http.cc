a21cba0da0c49379964995e677c3fa26542fa473
Tomas Janousek <tomi@nomi.cz>
2004-09-12 10:43:03 +0000

    No! We do not support HTTP/1.1 :)
    git-svn-id: file:///tmp/svn/TomiTCP/head@117 0366d8a9-9ecf-0310-8b56-9fddfa06f24e

--- TomiTCP/http.cc
+++ TomiTCP/http.cc
@@ -59,7 +59,7 @@ namespace net {
 	}
 
 	fprintf(stream,
-		"GET %s HTTP/1.1\r\n"
+		"GET %s HTTP/1.0\r\n"
 		"User-Agent: TomiTCP/" PACKAGE_VERSION "\r\n"
 		"Host: %s\r\n"
 		"Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5\r\n"
@@ -162,7 +162,7 @@ namespace net {
 	}
 
 	fprintf(stream,
-		"POST %s HTTP/1.1\r\n"
+		"POST %s HTTP/1.0\r\n"
 		"User-Agent: TomiTCP/" PACKAGE_VERSION "\r\n"
 		"Host: %s\r\n"
 		"Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5\r\n"
