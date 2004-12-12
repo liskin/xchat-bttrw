a052de2481c3234e6b281a0617e2dd5daea4ff6e
Tomas Janousek <tomi@nomi.cz>
2004-12-12 12:20:28 +0000

    005 numeric update
    git-svn-id: file:///tmp/svn/tomi/xchat-bttrw@332 b4258ba7-4dd6-0310-8e73-d8b1db2f4ef9
    
    :100644 100644 c58ac41... c09ecaa... M	gate.cc

--- gate.cc
+++ gate.cc
@@ -285,10 +285,12 @@ main_accept:
 				me, nick.c_str());
 			fprintf(*c, ":%s 004 %s :%s xchat-bttrw-" VERSION " 0 io\n",
 				me, nick.c_str(), me);
-			fprintf(*c, ":%s 005 %s :MODES=1 MAXTARGETS=1 NICKLEN=20 SAFELIST\n",
+			fprintf(*c, ":%s 005 %s MODES=1 MAXTARGETS=1 NICKLEN=20 SAFELIST"
+				" :are supported by this server\n",
 				me, nick.c_str());
-			fprintf(*c, ":%s 005 %s :CHANTYPES=# PREFIX=(o)@ CHANMODES=,,,"
-				" NETWORK=xchat.cz CASEMAPPING=ascii\n", me, nick.c_str());
+			fprintf(*c, ":%s 005 %s CHANTYPES=# PREFIX=(o)@ CHANMODES=,,,i"
+				" NETWORK=xchat.cz CASEMAPPING=ascii"
+				" :are supported by this server\n", me, nick.c_str());
 
 			fprintf(*c, ":%s 375 %s :- %s Message of the Day -\n", me,
 				nick.c_str(), me);
