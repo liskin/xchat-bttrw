37b798abfa0e1cc3174d007f64fbe91973b0580f
Tomas Janousek <tomi@nomi.cz>
2005-04-04 21:38:27 +0000

    Don't leave room on parse error, just send KICK, remove it from the list and let client rejoin...
    git-svn-id: file:///tmp/svn/tomi/xchat-bttrw@369 b4258ba7-4dd6-0310-8e73-d8b1db2f4ef9
    
    :100644 100644 ed27431... 1a1c8b3... M	/gate.cc

--- gate.cc
+++ gate.cc
@@ -760,7 +760,7 @@ main_accept:
 		    auto_ptr<EvRoomError> f((EvRoomError*)e.release());
 
 		    if (f->isfatal()) {
-			try { x->leave(f->getrid()); } catch (...) { }
+			x->rooms.erase(f->getrid());
 			fprintf(*c, ":%s KICK #%s %s :Error: %s\n", me,
 				f->getrid().c_str(), nick.c_str(),
 				f->str().c_str());
